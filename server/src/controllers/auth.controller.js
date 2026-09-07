f3e0f64eeeea02ab7905974e14f7a9a5beb66fae
Harsh-Kumar-Pandit <harshkumarpandit2004@gmail.com>
2026-09-07 12:16:17 +0530

    fix production auth cookie

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -43,8 +43,8 @@ export const registerUser = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -107,8 +107,8 @@ export const loginUser = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -166,8 +166,8 @@ export const logoutUser = async (req, res) => {
     try {
         res.clearCookie("token", {
             httpOnly: true,
-            secure: false,
-            sameSite: "lax"
+            secure: true,
+            sameSite: "none"
         });
         return res.status(200).json({
             success: true,
@@ -200,8 +200,8 @@ export const searchUsers = async (req, res) => {
                 }
             ]
         })
-        .select("fullName username email avatar")
-        .limit(8);
+            .select("fullName username email avatar")
+            .limit(8);
 
         let workspace = null;
         let pendingInvites = [];
